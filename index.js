16e4e8860be18d15f1bb747f24bfb408f17f6697
Alex Gicimo Gakuru <gakurualexgicimo@gmail.com>
2022-08-16 23:44:01 +0300

    commit tree 57f9cde81a95bbb7361c44b0cf4a984d971c1d04

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // Code your solution in this file!
+
+const returnFirstTwoDrivers=(myArray)=>(myArray.slice(0,2));
+
+const returnLastTwoDrivers =(myArray)=>(myArray.slice(-2));
+
+const selectingDrivers=[returnFirstTwoDrivers,returnLastTwoDrivers];
+
+function createFareMultiplier(number){
+    const cb=(fare)=>(fare*number);
+
+    return cb;
+}
+
+
+const  fareDoubler =(fare)=>(createFareMultiplier(2)(fare));
+
+const  fareTripler =(fare)=>(createFareMultiplier( 3)(fare));
+
+
+
+console.log(fareDoubler(10));
+
+function selectDifferentDrivers(drivers,returnFirstTwoDrivers){
+
+
+
+return returnFirstTwoDrivers(drivers);
+}
+
